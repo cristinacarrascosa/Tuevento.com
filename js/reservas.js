1bd8896a923411d9cb934cd678363b1ea7ddfdb1
ianmgdaw <ianmg.daw@gmail.com>
2022-06-06 20:39:21 +0200

    Modificado los comentarios

--- js/reservas.js
+++ js/reservas.js
@@ -1,22 +1,18 @@
 //'use strict'
 
 // VARIABLES
+// Botones
 const btnNext1 = document.getElementById('btnNext1');
 const btnNext2 = document.getElementById('btnNext2');
 const btnToBook = document.getElementById('btnToBook');
 const btnDelete = document.getElementById('btnDelete');
-
 const btnDatosPers = document.getElementById('btnDatosPers');
 const btnEspacios = document.getElementById('btnEspacios');
 
+//div
 const formDatosPers = document.getElementById('form-datos-personales');
 const formEspacio = document.getElementById('form-espacio');
 const formGastronomia = document.getElementById('form-gastronomia');
-
-//console.log("ESTILO1:" + formDatosPers);
-//console.log("ESTILO2:" + formEspacio);
-//console.log("ESTILO3:" + formGastronomia);
-
 
 // VARIABLES PARA CAMPOS
 const nombreApellidos = document.querySelector('#nombreApellidos');
@@ -40,8 +36,6 @@ function iniciarApp() {
     // Inicio de la aplicación y oculta los div de formEspacio y formGastronomía
     formEspacio.style.display = 'none';
     formGastronomia.style.display = 'none';
-    //ocultarDivEspacio();
-    //ocultarDivGastronomia();
 }
 
 
