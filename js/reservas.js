13765c1d5b69947dc2f67a2faee4e2d10c4d0174
ianmgdaw <ianmg.daw@gmail.com>
2022-06-06 17:15:51 +0200

    Continua 'boton siguiente'

--- js/reservas.js
+++ js/reservas.js
@@ -1,10 +1,14 @@
+//'use strict'
+
 // VARIABLES
-const btnNext1 = document.querySelector('#btnNext1');
+const btnNext1 = document.getElementById('btnNext1');
 const btnNext2 = document.querySelector('#btnNext2');
 const btnToBook = document.querySelector('#btnToBook');
 const btnDelete = document.querySelector('#btnDelete');
 
+const formDatosPers = document.getElementById('form-datos-personales');
 const formEspacio = document.getElementById('form-espacio');
+const formGastronomia = document.getElementById('form-gastronomia');
 
 // VARIABLES PARA CAMPOS
 const nombreApellidos = document.querySelector('#nombreApellidos');
@@ -18,9 +22,49 @@ eventListeners();
 function eventListeners() {
     //Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+    console.log('Funciona 1');
+
+    //Campos del formulario
+
+
+    // Botón de 'Siguiente'
+    btnNext1.addEventListener('click', validarFormDatosPers);
+    console.log('Funciona 2');
 }
 
 function iniciarApp() {
-    console.log(formEspacio);
+    
+    // Inicio de la aplicación y oculta los div de formEspacio y formGastronomía
+    ocultarDivEspacio();
+    ocultarDivGastronomia();
+    
+}
+
+function ocultarDivDatosPers() {
+    console.log('OCULTAR DIV DATOS PERSONALES');
+    formDatosPers.style.display = 'none';
+}
+
+function ocultarDivEspacio() {
     formEspacio.style.display = 'none';
+}
+
+function ocultarDivGastronomia() {
+    formGastronomia.style.display = 'none';
+}
+
+function mostrarDivEspacio() {
+    formEspacio.style.display = '';
+}
+
+function mostrarDivGastronomia() {
+    formGastronomia.style.display = '';
+}
+
+
+function validarFormDatosPers() {
+    console.log('SIGUIENTEEEE');
+    ocultarDivDatosPers();
+    //mostrarDivEspacio();
+    //ocultarDivGastronomia();
 }
