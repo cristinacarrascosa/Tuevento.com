1fba26ad717f42862a75a3f808ac5c8353c15d86
ianmgdaw <ianmg.daw@gmail.com>
2022-06-06 20:35:19 +0200

    Div/form ocultar/mostrar TERMINADO

--- js/reservas.js
+++ js/reservas.js
@@ -2,19 +2,26 @@
 
 // VARIABLES
 const btnNext1 = document.getElementById('btnNext1');
-const btnNext2 = document.querySelector('#btnNext2');
-const btnToBook = document.querySelector('#btnToBook');
-const btnDelete = document.querySelector('#btnDelete');
+const btnNext2 = document.getElementById('btnNext2');
+const btnToBook = document.getElementById('btnToBook');
+const btnDelete = document.getElementById('btnDelete');
+
+const btnDatosPers = document.getElementById('btnDatosPers');
+const btnEspacios = document.getElementById('btnEspacios');
 
 const formDatosPers = document.getElementById('form-datos-personales');
 const formEspacio = document.getElementById('form-espacio');
 const formGastronomia = document.getElementById('form-gastronomia');
 
+//console.log("ESTILO1:" + formDatosPers);
+//console.log("ESTILO2:" + formEspacio);
+//console.log("ESTILO3:" + formGastronomia);
+
+
 // VARIABLES PARA CAMPOS
 const nombreApellidos = document.querySelector('#nombreApellidos');
 const email = document.querySelector('#email');
 const movil = document.querySelector('#movil');
-
 
 eventListeners();
 
@@ -22,49 +29,28 @@ eventListeners();
 function eventListeners() {
     //Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
-    console.log('Funciona 1');
-
-    //Campos del formulario
-
 
     // Botón de 'Siguiente'
     btnNext1.addEventListener('click', validarFormDatosPers);
-    console.log('Funciona 2');
+    btnNext2.addEventListener('click', validarFormEspacios);
+    
 }
 
 function iniciarApp() {
-    
     // Inicio de la aplicación y oculta los div de formEspacio y formGastronomía
-    ocultarDivEspacio();
-    ocultarDivGastronomia();
-    
-}
-
-function ocultarDivDatosPers() {
-    console.log('OCULTAR DIV DATOS PERSONALES');
-    formDatosPers.style.display = 'none';
-}
-
-function ocultarDivEspacio() {
     formEspacio.style.display = 'none';
-}
-
-function ocultarDivGastronomia() {
     formGastronomia.style.display = 'none';
-}
-
-function mostrarDivEspacio() {
-    formEspacio.style.display = '';
-}
-
-function mostrarDivGastronomia() {
-    formGastronomia.style.display = '';
+    //ocultarDivEspacio();
+    //ocultarDivGastronomia();
 }
 
 
 function validarFormDatosPers() {
-    console.log('SIGUIENTEEEE');
-    ocultarDivDatosPers();
-    //mostrarDivEspacio();
-    //ocultarDivGastronomia();
+    formEspacio.style.display = '';
+    btnDatosPers.style.display = 'none';
+}
+
+function validarFormEspacios() {
+    formGastronomia.style.display = '';
+    btnEspacios.style.display = 'none';
 }
